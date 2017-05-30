ca5e88f312238e2c8de41f4fe67b7e15449b50a3
Lion Yang <lion@aosc.xyz>
2017-05-30 15:37:13 +0800

    ciel: add command router and help msg

--- src/ciel/ciel.go
+++ src/ciel/ciel.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"ci"
+	"fmt"
 	"log"
 	"os"
 )
 
 const MachineName = "buildkit"
+const ShellPath = "/bin/bash"
 const ACBSPath = "/bin/acbs-build"
 
 func main() {
+	var cielrouter func(*ci.ContainerInstance, []string) error
+	switch {
+	case len(os.Args) == 1+1 && os.Args[1] == "shell":
+		cielrouter = cielshell
+	case len(os.Args) >= 1+2 && os.Args[1] == "run":
+		cielrouter = cielrun
+	case len(os.Args) >= 1+2 && os.Args[1] == "build":
+		cielrouter = cielbuild
+	case len(os.Args) == 1+0:
+		fallthrough
+	default:
+		cielhelp()
+		return
+	}
+
 	fs, err := ci.InitFilesystem("/root/ciel/buildkit") // FIXME: configurability
 	if err != nil {
 		log.Panicln("fs", err)
@@ -30,11 +47,41 @@ func main() {
 		}
 	}()
 
-	cmd := container.Exec(ACBSPath, "-c", os.Args[1]) // TODO: multi-package building
+	args := []string{}
+	if len(os.Args) >= 1+2 {
+		args = os.Args[2:]
+	}
+	if err := cielrouter(container, args); err != nil {
+		log.Panicln(err)
+	}
+}
+
+func cielhelp() {
+	fmt.Println(`
+commands:
+	shell       shell in container
+	run <cmd>   run command in container
+	build <pkg> build package in container
+	(default)   show this information
+`)
+}
+
+func cielshell(container *ci.ContainerInstance, args []string) error {
+	cmd := container.Exec(ShellPath)
+	redirectStdOutErr(cmd)
+	return cmd.Run()
+}
+func cielrun(container *ci.ContainerInstance, args []string) error {
+	cmd := container.Exec(os.Args[2:]...)
+	redirectStdOutErr(cmd)
+	return cmd.Run()
+}
+func cielbuild(container *ci.ContainerInstance, args []string) error {
+	cmd := container.Exec(ACBSPath, "-c", os.Args[2]) // TODO: multi-package building
 	redirectStdOutErr(cmd)
 	if err := cmd.Run(); err != nil {
-		log.Panicln("acbs-build", err)
+		return err
 	}
-
 	// TODO: pick up the package; collect acbs-build log, autobuild log ...
+	return nil
 }
