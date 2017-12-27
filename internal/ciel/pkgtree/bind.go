70b783e72446b8aad60d801b5cfe23956c59b3b5
Lion Yang <lion@aosc.io>
2017-12-27 20:14:06 +0800

    tree/mount: check if there is tree

--- internal/ciel/pkgtree/bind.go
+++ internal/ciel/pkgtree/bind.go
@@ -8,11 +8,17 @@ import (
 )
 
 func (t *Tree) Mount(mountPoint string) {
+	if _, err := os.Stat(t.BasePath); os.IsNotExist(err) {
+		return
+	}
 	os.MkdirAll(mountPoint, 0755)
 	syscall.Mount(t.BasePath, mountPoint, "", syscall.MS_BIND, "")
 }
 
 func (t *Tree) Unmount(mountPoint string) {
+	if _, err := os.Stat(t.BasePath); os.IsNotExist(err) {
+		return
+	}
 	d.ITEM("unmount tree")
 	syscall.Unmount(mountPoint, 0)
 	d.OK()
