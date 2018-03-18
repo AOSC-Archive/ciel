d46c1a05e7edaeb9a4af32070094d3a9bd4a0b84
Lion Yang <lion@aosc.io>
2018-03-18 18:43:44 +0800

    list: strip a tab

--- status.go
+++ status.go
@@ -15,7 +15,7 @@ func list() {
 	i.Check()
 	c := i.Container()
 
-	fmt.Printf("\t%s\t%s\t%s\t%s\n", "INSTANCE", "WORKDIR", "STATUS", "BOOT")
+	fmt.Printf("%s\t%s\t%s\t%s\n", "INSTANCE", "WORKDIR", "STATUS", "BOOT")
 	for _, inst := range c.GetAll() {
 		showInst := inst.Name
 		tabs := "\t\t"
@@ -27,27 +27,27 @@ func list() {
 		}
 		var fsStatus, ctnStatus, boot string
 		if inst.Running() {
-			ctnStatus = d.C0(d.CYAN, "running")
+			ctnStatus = d.C0(d.GREEN, "running")
+			if inst.RunningAsBootMode() {
+				boot = d.C(d.CYAN, "yes")
+			} else {
+				boot = d.C(d.PURPLE, "no")
+			}
 		} else {
-			ctnStatus = d.C0(d.GREEN, "offline")
-		}
-		if inst.RunningAsBootMode() {
-			boot = d.C0(d.CYAN, "yes")
-		} else {
-			boot = d.C0(d.WHITE, "no")
+			ctnStatus = d.C0(d.WHITE, "offline")
 		}
 		if inst.Mounted() {
 			fsStatus = d.C0(d.GREEN, "mounted")
 		} else {
 			fsStatus = "free"
 		}
-		fmt.Printf("\t%s%s%s\t%s\t%s\n", showInst, tabs, fsStatus, ctnStatus, boot)
+		fmt.Printf("%s%s%s\t%s\t%s\n", showInst, tabs, fsStatus, ctnStatus, boot)
 	}
 	fmt.Println()
 	count := len(c.GetAllNames())
 	if count <= 1 {
-		fmt.Printf("\t%d instance listed.\n", count)
+		fmt.Printf("%d instance listed.\n", count)
 	} else {
-		fmt.Printf("\t%d instances listed.\n", count)
+		fmt.Printf("%d instances listed.\n", count)
 	}
 }
