3ca9565231676b8bb76942b3cc34e316a0121b74
liushuyu <liushuyu011@gmail.com>
2019-12-18 17:13:40 -0700

    _config.go: lint

--- _config.go
+++ _config.go
@@ -1,5 +1,7 @@
+// THIS FILE IS AUTO-GENERATED. DO NOT EDIT!
 package main
 
+// Automatically generated version and installation location
 const (
 	Version = "__VERSION__"
 	Prefix  = "__PREFIX__"
